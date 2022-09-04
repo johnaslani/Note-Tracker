4a51caaf2928337756df6764f31b5eede85d7f8e
johnaslani <aslani.john@gmail.com>
2022-09-03 19:11:15 -0700

    Refactor the code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,15 @@
 const fs = require("fs");
 const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
-const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+const {
+  readAndAppend,
+  readFromFile,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
-// // GET Route for retrieving all the noparstes
-// app.get("/notes", (req, res) =>
-//   // console.log("Get Notes: Call router")
-//   readFromFile("./db/db.json", "utf-8").then((data) => {
-//     console.log("")
-//   res.json(JSON.parse(data)))
-//     }
-// );
+// GET Route for retrieving all the noparstes
 router.get("/notes", (req, res) => {
-  readFromFile("db/db.json", "utf-8").then((data) => {
+  readFromFile("./db/db.json", "utf-8").then((data) => {
     let parseNotes;
     try {
       parseNotes = [].concat(JSON.parse(data));
@@ -25,9 +22,8 @@ router.get("/notes", (req, res) => {
 });
 
 // POST Route for submitting notes
-
 router.post("/notes", (req, res) => {
-  //   // Destructuring assignment for the items in req.body
+  // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
   // If all the required properties are present
@@ -36,9 +32,10 @@ router.post("/notes", (req, res) => {
     const newnote = {
       title,
       text,
-      notes_id: uuidv4(),
+      id: uuidv4(),
     };
 
+    // Read new note and add to the db.json file
     readAndAppend(newnote, "./db/db.json");
 
     const response = {
@@ -52,15 +49,17 @@ router.post("/notes", (req, res) => {
   }
 });
 
-router.delete("/notes/:notes_id", (req, res) => {
-  console.log("notes_id", req.params.notes_id);
-  // reading notes form db.json
-  let db = JSON.parse(fs.readFileSync("db/db.json"));
-  // removing note with id
-  let deleteNotes = db.filter((item) => item.id !== req.params.notes_id);
-  // Rewriting note to db.json
-  fs.writeFileSync("db/db.json", JSON.stringify(deleteNotes));
-  res.json(deleteNotes);
+// Delete a selected note
+router.delete("/notes/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("id", id);
+  readFromFile("./db/db.json", "utf-8").then((results) => {
+    const notes = JSON.parse(results);
+    const filterNotes = notes.filter((note) => note.id !== id);
+    writeToFile("./db/db.json", filterNotes);
+    // res.status(200);
+    res.json(notes);
+  });
 });
 
 module.exports = router;
